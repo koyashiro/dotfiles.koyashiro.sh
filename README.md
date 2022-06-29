9a9c84d109ecd0980aabb87b68e1426e169b6f14
koyashiro <develop@koyashi.ro>
2022-06-30 00:38:05 +0900

    chore: add `koyashiro.sh`

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # koyashiro.sh
 
-## dotfiles.koyashiro.sh
-
-koyashiro's [dotfiles](https://github.com/koyashiro/dotfiles) install script.
-
 ```sh
-curl -fsS https://dotfiles.koyashiro.sh | sh
+curl -fsS https://koyashiro.sh | sh
 ```
---
+++ koyashiro.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env sh
+
+set -eu
+
+cat <<'EOF'
+# koyashiro.sh
+
+## dotfiles.koyashiro.sh
+
+koyashiro's [dotfiles](https://github.com/koyashiro/dotfiles) install script.
+
+```sh
+curl -fsS https://dotfiles.koyashiro.sh | sh
+```
+EOF
